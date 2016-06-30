a9baedbbee3a27de1711389c4a5482bd17a45ff0
Aurelien Garret <aurelien.garret@gmail.com>
2016-06-30 21:47:42 +0200

    Simplify call to facette and personnalite

--- api/app/controllers/reponse.js
+++ api/app/controllers/reponse.js
@@ -26,19 +26,12 @@ ReponseController.pushData = function(req, res){
     Utils.info("Form are pushed to the server.");
 
     if (req.body) {
-        ReponseController.calcFacette(req.body.id_personne, req.body.reponses);
+        var f = new Facette(p, data);
+        f.calc();
+        var p = new Personnalite(p, data);
+        p.calc();
     } else {
         Utils.info("No body present in pushed data.");
     }
 };
-
-ReponseController.calcFacette = function(p, data) {
-    var f = new Facette(p, data, ReponseController.calcPersonnalite);
-    f.calc();
-}
-
-ReponseController.calcPersonnalite = function(p, data) {
-    var p = new Personnalite(p, data);
-    p.calc();
-}
 
