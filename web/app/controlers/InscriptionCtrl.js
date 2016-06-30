f5a50d359ee6db29f135e9c5531fd9c4e4e6cdc4
LeKerss <annas9001@gmail.com>
2016-06-30 12:16:18 +0200

    WEB - Added controller for inscription

--- web/app/controlers/InscriptionCtrl.js
+++ web/app/controlers/InscriptionCtrl.js
@@ -1,3 +1,15 @@
 /**
  * Created by Kersa on 30/06/2016.
  */
+'use strict';
+var current_resume;
+angular.module('thinktwiceApp')
+    .controller('inscriptionCtrl', function($scope, $http, $mdDialog, $mdMedia){
+
+        $scope.inscription = function(user) {
+            alert('toto');
+        }
+
+
+
+    });
