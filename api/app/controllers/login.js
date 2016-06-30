b7b9a12228ed85e60270f27c21dcac139f37b719
Aurelien Garret <aurelien.garret@gmail.com>
2016-06-30 18:47:55 +0200

    API - /login 2

--- api/app/controllers/login.js
+++ api/app/controllers/login.js
@@ -1,3 +1,47 @@
 /**
  * Created by aureliengarret on 30/06/2016.
  */
+
+var LoginController = exports;
+
+
+
+
+//--------------------------------------- Module dependencies.
+
+var     moment      = require('moment'),
+        Utils        = require('../helpers/appUtils');
+
+
+
+LoginController.connect = function(req, res){
+    Utils.info("Connection needed");
+
+    console.log(req.body);
+
+    if (req.body) {
+        var con = global.con();
+
+        var query = "select count(*) as isok, id from personne where login = ? and mdp = ?";
+
+        con.query(query, [req.body.login, req.body.mdp], function(err, rows) {
+            if (err || !rows[0].isok) {
+                res.status(403).json({message : 'User not found'});
+            }
+
+            var token = new Date().getTime();
+
+            var update = "update personne set token = ? where id = ?";
+            con.query(update, [token, rows[0].id], function(err, rows2) {
+                res.status(200).json({"id": rows[0].id, "token" : token });
+            })
+
+
+        });
+    } else {
+        res.status(403).json({message : 'Parameter not found'});
+    }
+
+
+};
+
--- api/app/routes/login.js
+++ api/app/routes/login.js
@@ -1,3 +1,15 @@
 /**
  * Created by aureliengarret on 30/06/2016.
  */
+
+'use strict';
+
+module.exports = function(router) {
+    //------ LOAD CONTROLLER
+    var loginController = require('../controllers/login'),
+        auth              = require('../helpers/auth');
+
+    // -------- Retrieve all person from the database
+    router.post('/login', loginController.connect);
+
+};
