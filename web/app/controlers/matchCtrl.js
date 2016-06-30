249e718e20b6371ca03b09dba2be12319170b01f
Antoine Drouhin <antoine.drouhin@gmail.com>
2016-06-30 09:46:23 +0200

    Front on work

--- web/app/controlers/matchCtrl.js
+++ web/app/controlers/matchCtrl.js
@@ -4,6 +4,14 @@ angular.module('thinktwiceApp')
     .controller('matchCtrl', function($scope, Resumes, $http, $mdDialog, $mdMedia){
 
 
+        io.sockets.on('connection', function (socket) {
+            socket.emit('msg', { msg: msg });
+            socket.on('msg', function(msg){
+                msg[msg.msg-1].msg++;
+                io.sockets.emit('msg', { msg: msg });
+            })
+        });
+
 
 
     });
