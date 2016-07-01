2050ee0d0c1d1e52f457e2d7ffe8632962cf0eb1
Aurelien Garret <aurelien.garret@gmail.com>
2016-07-01 19:23:33 +0200

    API - /Facette

--- api/app/controllers/facette.js
+++ api/app/controllers/facette.js
@@ -1,3 +1,30 @@
 /**
  * Created by aureliengarret on 01/07/2016.
  */
+var FacetteController = exports;
+
+
+//--------------------------------------- Module dependencies.
+
+var     moment      = require('moment'),
+        Utils        = require('../helpers/appUtils');
+
+
+FacetteController.get = function(req, res){
+
+
+    var query = "select * from facette";
+
+    var con = global.con();
+
+    con.query(query, function(err, rows) {
+        if(err) {
+            console.log(err);
+            res.status(400).json({error : true});
+            return;
+        }
+
+        res.status(200).json(rows);
+    })
+
+};
--- api/app/routes/facette.js
+++ api/app/routes/facette.js
@@ -1,3 +1,15 @@
 /**
- * Created by aureliengarret on 01/07/2016.
+ * Created by aureliengarret on 30/06/2016.
  */
+
+'use strict';
+
+module.exports = function(router) {
+    //------ LOAD CONTROLLER
+    var facetteController = require('../controllers/facette'),
+        auth              = require('../helpers/auth');
+
+    // -------- Retrieve all person from the database
+    router.get('/facette', auth.isAuth, facetteController.get);
+
+};
